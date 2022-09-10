ba5a46f3b6481fd765dde48699d57edc903893c3
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-09-10 16:21:51 -0700

    added tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char mystring[] = "Programming is like building a multilingual puzzle";
+
+	puts(mystring);
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: 0 always
+*/
+int main(void)
+{
+	char mystr[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", mystr);
+	return (0);
+}
