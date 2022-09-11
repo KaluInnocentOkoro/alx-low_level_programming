1f8f87f73ed480e98b6a3268715ba29ab025783d
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-09-11 10:18:21 -0700

    add task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Returns 0
+*/
+int main(void)
+{
+	int i = 97;
+	int j = 65;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	while (j <= 90)
+	{
+		putchar(j);
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
