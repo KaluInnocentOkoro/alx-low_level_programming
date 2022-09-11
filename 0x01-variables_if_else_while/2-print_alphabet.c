4268e48c6c0ce7d8c995fe93ccfcbbdcfaaa09cb
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-09-11 10:07:03 -0700

    addbtask 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Returns 0
+*/
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
