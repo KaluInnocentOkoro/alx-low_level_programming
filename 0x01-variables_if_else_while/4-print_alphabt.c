f7c4045b3c34093ba5d88fcfc836b0570fb20d1a
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-09-11 10:48:08 -0700

    add task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - Entry point of the program
+*
+* Return: Return 0
+*/
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		if (i == 101 || i == 113)
+			continue;
+		else
+			putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
