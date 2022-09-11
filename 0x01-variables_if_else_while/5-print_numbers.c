973c3440c426e745aae42847b7f24fcb78c1ded1
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-09-11 11:03:38 -0700

    add task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main program
+*
+* Return: 0
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
