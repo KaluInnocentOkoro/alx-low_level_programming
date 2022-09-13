ae10fd72f1f544ce92e0e2db9608c008403d6131
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-09-13 05:06:23 +0100

    message

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point of program
+*
+* Return - 0
+*/
+int main(void)
+{
+	int i = 48;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i == 56 && j == 57)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
