447b505b23e9a0d13ffce908888c653bf05bd499
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-09-21 04:44:29 +0100

    message

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,11 @@
 */
 void print_rev(char *s)
 {
-	int len;
+	int len, i;
 
 	for (len = 0; s[len]; len++)
 		;
-	while (len - 1 >= 0)
-	{
-		_putchar(s[len]);
-		len--;
-	}
+	for (i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
