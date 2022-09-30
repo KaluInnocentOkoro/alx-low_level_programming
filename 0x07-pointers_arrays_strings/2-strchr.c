3adf99e8e0963f452447136da3a4b7d9fbaed526
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-09-30 23:56:16 +0100

    message

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
 * _strchr - function locates a character in a string.
 * @s: String in which a character is to be located
-* c: character to be searched for
+* @c: character to be searched for
 *
 * Return: a pointer to the first occurrence of the character c in the string s,
 * or NULL if the character is not found
