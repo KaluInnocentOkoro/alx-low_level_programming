35299e8b35fc59827137b289ce1f38d0568369a7
Kalu Innocent Okoro <kaluinnocentokoro@gmail.com>
2022-10-02 04:22:05 -0700

    updated task

--- 0x09-static_libraries/2-strchr.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept)
-{
-	;
-}
--- 0x09-static_libraries/3-strcmp.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n)
-{
-	;
-}
