a2bde6ed8f94fc0d5f6303c73ade282575144f7a
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-10-02 19:58:59 +0100

    updated tasks

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - main function's and header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -21,5 +31,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
