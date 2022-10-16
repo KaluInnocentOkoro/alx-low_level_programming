aceaa924ff142a6b3781a8f6a3f75c8b0bd4984d
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-10-16 14:37:27 +0100

    message

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,23 +16,23 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	 n1 = atoi(argv[1]);
-	 op = argv[2];
-	 n2 = atoi(argv[3]);
+	n1 = atoi(argv[1]);
+	op = argv[2];
+	n2 = atoi(argv[3]);
 
-	 if (get_op_func(op) == NULL || op[1] != '\0')
-	 {
-		 printf("Error\n");
-		 exit(99);
-	 }
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-	 if ((*op == '/' && n2 == 0) ||
+	if ((*op == '/' && n2 == 0) ||
 		(*op == '%' && n2 == 0))
-	 {
-		 printf("Error\n");
-		 exit(100);
-	 }
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-	 printf("%d\n", get_op_func(op)(n1, n2));
+	printf("%d\n", get_op_func(op)(n1, n2));
 	return (0);
 }
