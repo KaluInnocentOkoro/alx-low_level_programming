b73c8a1a8ebcb12b9fc4f89b0515f162ea7da624
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-10-26 05:43:07 +0100

    message

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
+		return;
 	while ((*head) != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
-	*head = NULL;
+	head = NULL;
 }
